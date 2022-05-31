b04b5ef66b4191806700e16b4e548a270b523769
Mark <mark_gr.07@mail.ru>
2022-05-31 09:43:47 +0300

    Переделал 3 урок

--- script.js
+++ script.js
@@ -1,41 +1,35 @@
 
+'use strict';
+
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice2 = +prompt("Сколько это будет стоить?");
-console.log(servicePrice2);
 
-const fullPrice = screenPrice + servicePrice1 + servicePrice2;
-console.log(fullPrice);
+let rollback = 10;
 
-let rollback = 50;
-const servicePercentPrice = fullPrice - (rollback/100);
-console.log(Math.ceil(servicePercentPrice));
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = fullPrice - (fullPrice * (rollback/100));
 
-if (fullPrice > 30000) {
+if (fullPrice >= 30000) {
     console.log("Даем скидку в 10%");
-}else if (15000 < fullPrice && fullPrice < 30000) {
+}else if (fullPrice >= 15000 && fullPrice < 30000) {
     console.log("Даем скидку в 5%");
-}else if (0 < fullPrice && fullPrice <= 15000) {
+}else if (fullPrice >= 0 && fullPrice < 15000) {
     console.log("Скидка не предусмотрена");
 }else {
     console.log("Что-то пошло не так");
 }
 
+console.log(typeof title);
+console.log(typeof screenPrice);
+console.log(typeof adaptive);
 
-/*
-не понимаю, почему при вводе любого числа в интервале 0-31к, выдаёт 'что-то пошло не так'. Я понимаю, что в каком-то тупом 
-месте не замечаю ошибку, но не понимаю, где
+console.log(screens.length);
+console.log(servicePercentPrice);
 
-let fullPrice = 31000;
-switch (fullPrice) {
-    case fullPrice > 30000:
-        console.log("Даем скидку в 10%");  
-        break
-    case 15000 <= fullPrice && fullPrice <= 30000:
-        console.log("Даем скидку в 5%"); 
-        break
-    case 0 < fullPrice && fullPrice <= 15000:
-        console.log("Скидка не предусмотрена");
-        break
-    default:
-        console.log("Что-то пошло не так");
-}
-*/ 
+console.log("Стоимость вёрстки экранов " + screenPrice + " рублей/ долларов/ гривен/ юаней");
